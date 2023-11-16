3f763d4af319512bee21a66a916da6fd08ac4f95
Jerry Wu <953855668@qq.com>
2023-11-16 18:02:53 +0800

    Add Projection 2d&3d.

--- src/draw.py
+++ src/draw.py
@@ -1,7 +1,41 @@
 from data_loader import image_to_mat
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 from config import image_format, train_dir, test_dir, output_fig_dir
+
+
+def draw_faces(faces_mat, sub_width=None, save_fig=False, name=None):
+    """
+    Draw the faces of the dataset.
+    :param name: filename of the output figure.
+    :param sub_width: fig subplot width, number of cols.
+    :param save_fig: whether save the face file.
+    :param faces_mat: 2D array of images, number * 32 * 32.
+    :return: figure
+    """
+    length = math.ceil(faces_mat.shape[0] / sub_width)
+    fig, ax = plt.subplots(length, sub_width)  # , figsize=(10, 10))
+
+    for i in range(length * sub_width):
+        if length == 1:
+            axi = ax[i]
+        else:
+            row = i // sub_width
+            col = i - row * sub_width
+            axi = ax[row, col]
+
+        if i + 1 <= faces_mat.shape[0]:
+            axi.imshow(faces_mat[i], cmap='gray')
+            axi.axis('off')
+            axi.set_title(i+1)
+        else:
+            axi.set_visible(False)
+
+    plt.show()
+    if save_fig:
+        fig.savefig(output_fig_dir + '{0}Faces.png'.format(name + ': '))
+    return fig
 
 
 def draw_mean_face(mat, save_fig=False):
@@ -40,7 +74,7 @@ def draw_PCs_faces(reduced_eigen_faces, save_fig=False):
 
         ax.imshow(image_data, cmap='gray')
         ax.axis('off')
-        name = 'Eigen Face #{0}'.format(i + 1)
+        name = '{1}PCs: Eigen Face #{0}'.format(i + 1, num_PCs)
         ax.set_title(name)
         plt.show()
         if save_fig:
@@ -51,4 +85,7 @@ if __name__ == "__main__":
     image_dir = train_dir
 
     train_image, new_labels, label_mapping = image_to_mat(image_dir=image_dir, target_num=500, use_selfie=True)
-    draw_mean_face(train_image)
+    # draw_mean_face(train_image)
+
+    selfie_indices = np.where(np.array(new_labels) == 25)[0]
+    draw_faces(faces_mat=np.array(train_image)[selfie_indices], sub_width=4, save_fig=False, name=None)
